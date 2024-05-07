7f5185569094f4338e19052f6305d6ea6ca24b6d
Slava <slava@mail.ru>
2024-05-07 21:12:00 +0700

    2

--- main.js
+++ main.js
@@ -13,53 +13,67 @@ const header = document.querySelector(".header");
 const form = document.querySelector("#form");
 const input = document.querySelector("#city");
 
-// Слушаем отправку формы (можно нажать на кнопку, а можно Enter - и то, и то отправка)
-// По-хорошему этот блок кода лучше разбить на отдельные функции, в том числе - отдельная асинхронная для запроса, отдельно для принятия данных и формирования разметки и т.д.
-form.onsubmit = function (e) {
-  // Отменяем отправку формы
-  e.preventDefault();
-  let city;
-  city = input.value.trim();
+// Функция удаления карточки
+function removeCard() {
+  const prevCard = document.querySelector(".card");
+  if (prevCard) prevCard.remove();
+}
 
+function showError(errorMessage) {
+  const html = `<div class="card">${errorMessage}</div>`;
+  header.insertAdjacentHTML("afterend", html);
+}
+
+async function getWeather(city) {
   // Адрес запроса
   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showCard({ name, country, temp, condition }) {
+  // Разметка для карточки
+  const html = ` <div class="card">
+   <div class="card-city">
+     <h2 class="card-city">${name} <span>${country}</span></h2>
+   </div>
+   <div class="card-weather">
+     <div class="card-value">${temp}<sup>°c</sup></div>
+     <img src="weather.png" alt="weather" class="card-image" />
+   </div>
+
+   <div class="card-description">${condition}</div>
+ </div>`;
+  //  Отображаем карточку на странице
+  header.insertAdjacentHTML("afterend", html);
+}
+
+// Слушаем отправку формы (можно нажать на кнопку, а можно Enter - и то, и то отправка)
+// По-хорошему этот блок кода лучше разбить на отдельные функции, в том числе - отдельная асинхронная для запроса, отдельно для принятия данных и формирования разметки и т.д.
+form.onsubmit = async function (e) {
+  // Отменяем отправку формы
+  e.preventDefault();
+  let city = input.value.trim();
+
+  const data = await getWeather(city);
 
   // Выполняем запрос
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      // Проверка на ошибку
-      if (data.error) {
-        const html = `<div class="card">${data.error.message}</div>`;
-        const prevCard = document.querySelector(".card");
-        if (prevCard) prevCard.remove();
-        header.insertAdjacentHTML("afterend", html);
-      } else {
-        // если ошибки нет - выводим карточку, удаляем предыдущую
-        const prevCard = document.querySelector(".card");
-        if (prevCard) prevCard.remove();
-        console.log(data);
-        console.log(data.location.name);
-        console.log(data.location.country);
-        console.log(data.current.temp_c);
-        console.log(data.current.condition.text);
-        // Разметка для карточки
-        const html = ` <div class="card">
-          <div class="card-city">
-            <h2 class="card-city">${data.location.name} <span>${data.location.country}</span></h2>
-          </div>
-          <div class="card-weather">
-            <div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
-            <img src="weather.png" alt="weather" class="card-image" />
-          </div>
-  
-          <div class="card-description">${data.current.condition.text}</div>
-        </div>`;
-
-        //  Отображаем карточку на странице
-        header.insertAdjacentHTML("afterend", html);
-      }
-    });
+  // Проверка на ошибку
+  if (data.error) {
+    // если ошибка есть - выводим
+    removeCard();
+    showError(data.error.message);
+  } else {
+    // если ошибки нет - выводим карточку, удаляем предыдущую
+    removeCard();
+    const weatherData = {
+      name: data.location.name,
+      country: data.location.country,
+      temp: data.current.temp_c,
+      condition: data.current.condition.text,
+    };
+    showCard(weatherData);
+  }
 };
